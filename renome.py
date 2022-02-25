c01c50a82d259dfde127278f2912d74d5da284d8
pedro <pedro_freires18@outlook.com>
2022-02-25 16:49:58 -0300

    Codigo com as duas funçoes funcionando, analisar bugs

--- renome.py
+++ renome.py
@@ -8,26 +8,29 @@ relatorio1.pdf
 relatorio2.pdf
 relatorio4.pdf 
 
-ele irá identificar que não há o relatorio 3 e renomeiará o relatorio 4 para relatorio 3.
+ele irá identificar que não há o relatorio 3 e renomeiará o relatorio 4 para relatorio 3
+e assim por diante, corrigindo a sequencia.
 """
 
 
 import os, re
+from sys import exit
 import shutil as sh
+from random import randint
 
 
 def gerar_arq():
     
-    """GERAR ARQUIVOS PARA FAZER O SCRIPT RENOMEIAR"""
+    """GERAR ARQUIVOS PARA FAZER O SCRIPT RENOMEIAR, SÃO ARQUIVOS DE TESTE"""
 
     print(os.getcwd())
     for i in range(10):
-        with open(f'popli{i}.png',mode='a') as file:
+        with open(f'popli{randint(5,20)}.png',mode='a'):
             print('ok')
 
     return
 
-def reorganize(prefixo,exten):
+def sequencia(prefixo,exten):
     
     """RECONHECER A NUMERAÇÃO DOS ARQUIVOS"""
 
@@ -42,7 +45,7 @@ def reorganize(prefixo,exten):
     rage_corre = range(len(corres))
     corres = [list(corres[i]) for i in rage_corre] # transforma as tuplas em lista 
     corres = [[corres[i][0], int(corres[i][1])] for i in rage_corre] # transforma o i em um valor inteiro
-
+    corres = sorted(corres, key=lambda corres: corres[1]) # sort by value of file 
     
     return corres
 
@@ -53,15 +56,39 @@ def arquivo():
     prefix = str(input('Digite o prefixo do arquivo\nExemplo :: (relatorio)1, (relatorio)2\n::'))
     exten = str(input('Digite a extensão do arquivo sem o ponto\nExemplo :: .zip, .pdf, .png\n::'))
     
-    return prefix,exten
+    while True:
+    
+        esco = str(input('Zerar a numeração ou continuar do valor minimo dos arquivos\nZ: Zerar\nC: Continuar\n[Z/C]::'))
+        
+        if esco not in ['z','c']:
+            print('Digite um valor entre [Z ou C] para ditar como a ordem deve ser feita')
+            continue
+        
+        break
+    
+    return prefix,exten, esco
 
-def ordenar(valores):
+def ordenar(valores, desejo):
 
     """ORDENANDO OS ARQUIVOS COM A NUMERAÇÃO EM ORDEM"""
 
     # identificar a numeração correta ::
     rage_values = range(len(valores))
-    correct_sequ = [valores[i][1] + 1 for i in rage_values]
+    correct_sequ =[]
+
+    for i in rage_values:
+
+        if desejo == 'c' and i == 0: # continuando a ordem pelo menor valor dos arquivos
+                correct_sequ.append(valores[i][1])
+                continue           
+
+        elif desejo == 'z' and i == 0: # zerar o min valor e recomeçar  
+                valores[i][1] = 0
+                correct_sequ.append(valores[i][1] + 1 )
+                continue
+                
+        correct_sequ.append(correct_sequ [i-1] + 1)
+
     valores = [[valores[i][0], valores[i][1], correct_sequ[i]] for i in rage_values]
     del correct_sequ
 
@@ -79,7 +106,7 @@ def ordenar(valores):
 
     return valores
 
-def renomear(lista):
+def renomear(lista, desejo):
 
     """Função com o objetivo de renomear todos os arquivos com a sequencia correta"""
 
@@ -89,7 +116,8 @@ def renomear(lista):
     for i in rage_len:
     
         #substituindo utilizando regex, mas é possivel importar o prefixo e concatenar em uma string
-        correct_num_arq = re.sub(regex_subs, str(lista[i][1]), lista[i][0] , count=1)
+        
+        correct_num_arq = re.sub(regex_subs, str(lista[i][2]), lista[i][0] , count=1)
         # o re.sub funciona substituindo o pattern pelo repl na string !!
         lista[i].append(correct_num_arq)
         # movendo os arquivos para o diretorio atual com o nome trocado 
@@ -101,8 +129,11 @@ def renomear(lista):
 
 
 
+prefixo, extensao, esco = arquivo()
+lista = sequencia(prefixo, extensao)
 
-prefixo, extensao = arquivo()
-lista = reorganize(prefixo, extensao)
-lista = ordenar(lista)
-print(renomear(lista))
+if lista == False:
+    exit('Nenhum arquivo com o prefixo e extensão encontrado')
+
+lista = ordenar(lista, esco)
+print(renomear(lista, esco))
